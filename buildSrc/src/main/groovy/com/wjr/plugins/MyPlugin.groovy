ab278381b346d84fccc08e04d7a71719c8fad771
KeepYoung <wjr1203144058@gmail.com>
2020-08-01 14:42:57 +0800

    1.gradle plugin测试v10

--- buildSrc/src/main/groovy/com/wjr/plugins/MyPlugin.groovy
+++ buildSrc/src/main/groovy/com/wjr/plugins/MyPlugin.groovy
@@ -9,58 +9,57 @@ import org.gradle.api.file.ConfigurableFileTree
 
 // 飞书：https://open.feishu.cn/open-apis/bot/hook/38ce74e6df4b423c89de4bd028050482
 class MyPlugin implements Plugin<Project> {
+    String serverJenkinsSourcePath = "/Users/timedomain/.jenkins/workspace/"
     String serverJenkinsJobPath = "/Users/timedomain/.jenkins/jobs/"
-    String defaultArtifactsPath = "artifact/app/build/outputs/apk/release/"
+    String defaultApkPath = "/app/build/outputs/apk/release/"
+    String defaultArtifactsPath = "artifact${defaultApkPath}"
 
     @Override
     void apply(Project project) {
         project.task('pluginTest') {
             doLast {
-                new Thread(new Runnable() {
-                    @Override
-                    void run() {
-                        sleep(10000)
-                        def result = HttpBuilder.configure {
-                            request.uri = 'https://open.feishu.cn/open-apis/bot/hook/38ce74e6df4b423c89de4bd028050482'
-                            FeishuObj obj = new FeishuObj()
-                            def env = System.getenv()
-                            def buildUrl = env['BUILD_URL']
-                            String title = "项目：${env['JOB_NAME']} 版本：${env['BUILD_NUMBER']} "
-                            StringBuilder contentStr = new StringBuilder()
-                            ConfigurableFileTree fileTree = project.fileTree(
-                                    "${serverJenkinsJobPath}${env['JOB_NAME']}/${env['BUILD_NUMBER']}/${defaultArtifactsPath}")
-                            File t = new File("${serverJenkinsJobPath}${env['JOB_NAME']}/${env['BUILD_NUMBER']}/${defaultArtifactsPath}")
-                            println('文件测试：' + t.getPath() + "\n" + t.listFiles())
-                            if (fileTree == null || fileTree.size() == 0) {
-                                obj.title = title + "构建失败"
-                                obj.text = "详情请查看Log：${env['BUILD_URL']}console"
-                            } else {
-                                fileTree.each { File file ->
-                                    def filePrefixName
-                                    if (file.name.contains(".")) {
-                                        filePrefixName = file.name.substring(0, file.name.lastIndexOf("."))
-                                    } else {
-                                        filePrefixName = name
-                                    }
-                                    contentStr.append("\n")
-                                            .append("文件：")
-                                            .append(filePrefixName)
-                                            .append(" 下载地址为：")
-                                            .append(buildUrl)
-                                            .append(defaultArtifactsPath)
-                                            .append(file.name)
-                                }
-                                obj.title = title + "构建成功"
-                                obj.text = contentStr.toString()
-                            }
+                def result = HttpBuilder.configure {
+                    request.uri = 'https://open.feishu.cn/open-apis/bot/hook/38ce74e6df4b423c89de4bd028050482'
+                    FeishuObj obj = new FeishuObj()
 
-                            request.body = new Gson().toJson(obj)
-                            request.charset = 'UTF-8'
-                            request.contentType = 'application/json'
-                        }.post()
-                        println result
+                    def env = System.getenv()
+                    def buildUrl = env['BUILD_URL']
+                    def sourceApkPath = "${serverJenkinsSourcePath}${env['JOB_NAME']}/${defaultApkPath}"
+
+                    def fileTree = project.fileTree(sourceApkPath).findAll {
+                        it.name.endsWith(".apk")
                     }
-                }).start()
+
+                    File t = new File(sourceApkPath)
+                    println('文件测试：' + t.getPath() + "\n" + t.listFiles())
+
+                    String title = "项目：${env['JOB_NAME']} 版本：${env['BUILD_NUMBER']} 构建完成"
+                    StringBuilder contentStr = new StringBuilder()
+                    fileTree.each { File file ->
+                        def filePrefixName
+                        if (file.name.contains(".")) {
+                            filePrefixName = file.name.substring(0, file.name.lastIndexOf("."))
+                        } else {
+                            filePrefixName = name
+                        }
+                        contentStr.append("\n")
+                                .append("文件：")
+                                .append(filePrefixName)
+                                .append(" 下载地址为：")
+                                .append(buildUrl)
+                                .append(defaultArtifactsPath)
+                                .append(file.name)
+                    }
+                    contentStr.append("\n")
+                            .append("如果点击无效，请稍等几秒后打开，文件正在归档中")
+                    obj.title = title + "构建成功"
+                    obj.text = contentStr.toString()
+
+                    request.body = new Gson().toJson(obj)
+                    request.charset = 'UTF-8'
+                    request.contentType = 'application/json'
+                }.post()
+                println result
             }
         }
     }
