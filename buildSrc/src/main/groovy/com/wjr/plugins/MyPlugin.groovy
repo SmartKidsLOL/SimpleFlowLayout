d51fd23ee1e18f9f7805836ffe082d2ba20ec0b5
KeepYoung <wjr1203144058@gmail.com>
2020-08-01 12:32:26 +0800

    1.gradle plugin测试v8

--- buildSrc/src/main/groovy/com/wjr/plugins/MyPlugin.groovy
+++ buildSrc/src/main/groovy/com/wjr/plugins/MyPlugin.groovy
@@ -9,6 +9,7 @@ import org.gradle.api.file.ConfigurableFileTree
 
 // 飞书：https://open.feishu.cn/open-apis/bot/hook/38ce74e6df4b423c89de4bd028050482
 class MyPlugin implements Plugin<Project> {
+    String serverJenkinsJobPath = "/Users/timedomain/.jenkins/jobs/"
     String defaultArtifactsPath = "artifact/app/build/outputs/apk/release/"
 
     @Override
@@ -20,29 +21,34 @@ class MyPlugin implements Plugin<Project> {
                     FeishuObj obj = new FeishuObj()
                     def env = System.getenv()
                     def buildUrl = env['BUILD_URL']
-                    String title = "项目：${env['JOB_NAME']} 版本：${env['BUILD_NUMBER']} 已构建完成"
+                    String title = "项目：${env['JOB_NAME']} 版本：${env['BUILD_NUMBER']} "
                     StringBuilder contentStr = new StringBuilder()
                     ConfigurableFileTree fileTree = project.fileTree(
-                            "/Users/timedomain/.jenkins/job/${env['JOB_NAME']}/${env['BUILD_NUMBER']}/${defaultArtifactsPath}")
+                            "${serverJenkinsJobPath}${env['JOB_NAME']}/${env['BUILD_NUMBER']}/${defaultArtifactsPath}")
                     println('文件测试：' +
-                            new File("/Users/timedomain/.jenkins/").listFiles())
-                    fileTree.each { File file ->
-                        def filePrefixName
-                        if (file.name.contains(".")) {
-                            filePrefixName = file.name.substring(0, file.name.lastIndexOf("."))
-                        } else {
-                            filePrefixName = name
+                            new File("/Users/timedomain/.jenkins/jobs/").listFiles())
+                    if (fileTree == null || fileTree.size() == 0) {
+                        obj.title = title + "构建失败"
+                        obj.text = "详情请查看Log：${env['BUILD_URL']}console"
+                    } else {
+                        fileTree.each { File file ->
+                            def filePrefixName
+                            if (file.name.contains(".")) {
+                                filePrefixName = file.name.substring(0, file.name.lastIndexOf("."))
+                            } else {
+                                filePrefixName = name
+                            }
+                            contentStr.append("\n")
+                                    .append("文件：")
+                                    .append(filePrefixName)
+                                    .append(" 下载地址为：")
+                                    .append(buildUrl)
+                                    .append(defaultArtifactsPath)
+                                    .append(file.name)
                         }
-                        contentStr.append("\n")
-                                .append("文件：")
-                                .append(filePrefixName)
-                                .append(" 下载地址为：")
-                                .append(buildUrl)
-                                .append(defaultArtifactsPath)
-                                .append(file.name)
+                        obj.title = title
+                        obj.text = contentStr.toString()
                     }
-                    obj.title = title
-                    obj.text = contentStr.toString()
 
                     request.body = new Gson().toJson(obj)
                     request.charset = 'UTF-8'
