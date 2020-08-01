d61de14496cf8e5c60331afe9b69c3f37dd6cd1e
KeepYoung <wjr1203144058@gmail.com>
2020-08-01 11:43:05 +0800

    1.gradle plugin测试v4

--- buildSrc/src/main/groovy/com/wjr/plugins/MyPlugin.groovy
+++ buildSrc/src/main/groovy/com/wjr/plugins/MyPlugin.groovy
@@ -5,6 +5,7 @@ import com.wjr.plugins.models.FeishuObj
 import groovyx.net.http.HttpBuilder
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.ConfigurableFileTree
 
 // 飞书：https://open.feishu.cn/open-apis/bot/hook/38ce74e6df4b423c89de4bd028050482
 class MyPlugin implements Plugin<Project> {
@@ -15,16 +16,35 @@ class MyPlugin implements Plugin<Project> {
                 def result = HttpBuilder.configure {
                     request.uri = 'https://open.feishu.cn/open-apis/bot/hook/38ce74e6df4b423c89de4bd028050482'
                     FeishuObj obj = new FeishuObj()
-                    obj.title = "测试标题"
-                    obj.text = "测试内容"
+                    def env = System.getenv()
+                    def buildUrl = env['BUILD_URL']
+                    String title = "项目：${env['JOB_NAME']} 版本：${env[BUILD_NUMBER]} 已构建完成"
+                    StringBuilder contentStr = ""
+                    ConfigurableFileTree fileTree = project.fileTree('G:\\music')
+                    fileTree.each { File file ->
+                        def filePrefixName
+                        if (file.name.contains(".")) {
+                            filePrefixName = file.name.substring(0, file.name.lastIndexOf("."))
+                        } else {
+                            filePrefixName = name
+                        }
+                        contentStr.append("\n")
+                                .append("文件：")
+                                .append(filePrefixName)
+                                .append(" 下载地址为：")
+                                .append(buildUrl)
+                                .append(defaultArtifactsPath)
+                                .append(file.name)
+                    }
+                    obj.title = title
+                    obj.text = contentStr.toString()
+
                     request.body = new Gson().toJson(obj)
                     println(request.body)
                     request.charset = 'UTF-8'
                     request.contentType = 'application/json'
                 }.post()
                 println result
-                def env = System.getenv()
-                println(env)
             }
         }
     }
