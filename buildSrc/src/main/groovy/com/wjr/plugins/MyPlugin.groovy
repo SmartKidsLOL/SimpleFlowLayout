e7e85fb09071f54fb11113dbe124419a18fe2be3
KeepYoung <wjr1203144058@gmail.com>
2020-08-01 13:08:38 +0800

    1.gradle plugin测试v9

--- buildSrc/src/main/groovy/com/wjr/plugins/MyPlugin.groovy
+++ buildSrc/src/main/groovy/com/wjr/plugins/MyPlugin.groovy
@@ -16,45 +16,51 @@ class MyPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.task('pluginTest') {
             doLast {
-                def result = HttpBuilder.configure {
-                    request.uri = 'https://open.feishu.cn/open-apis/bot/hook/38ce74e6df4b423c89de4bd028050482'
-                    FeishuObj obj = new FeishuObj()
-                    def env = System.getenv()
-                    def buildUrl = env['BUILD_URL']
-                    String title = "项目：${env['JOB_NAME']} 版本：${env['BUILD_NUMBER']} "
-                    StringBuilder contentStr = new StringBuilder()
-                    ConfigurableFileTree fileTree = project.fileTree(
-                            "${serverJenkinsJobPath}${env['JOB_NAME']}/${env['BUILD_NUMBER']}/${defaultArtifactsPath}")
-                    File t = new File("${serverJenkinsJobPath}${env['JOB_NAME']}/${env['BUILD_NUMBER']}/${defaultArtifactsPath}")
-                    println('文件测试：' + t.getPath() + "\n" + t.listFiles())
-                    if (fileTree == null || fileTree.size() == 0) {
-                        obj.title = title + "构建失败"
-                        obj.text = "详情请查看Log：${env['BUILD_URL']}console"
-                    } else {
-                        fileTree.each { File file ->
-                            def filePrefixName
-                            if (file.name.contains(".")) {
-                                filePrefixName = file.name.substring(0, file.name.lastIndexOf("."))
+                new Thread(new Runnable() {
+                    @Override
+                    void run() {
+                        sleep(10000)
+                        def result = HttpBuilder.configure {
+                            request.uri = 'https://open.feishu.cn/open-apis/bot/hook/38ce74e6df4b423c89de4bd028050482'
+                            FeishuObj obj = new FeishuObj()
+                            def env = System.getenv()
+                            def buildUrl = env['BUILD_URL']
+                            String title = "项目：${env['JOB_NAME']} 版本：${env['BUILD_NUMBER']} "
+                            StringBuilder contentStr = new StringBuilder()
+                            ConfigurableFileTree fileTree = project.fileTree(
+                                    "${serverJenkinsJobPath}${env['JOB_NAME']}/${env['BUILD_NUMBER']}/${defaultArtifactsPath}")
+                            File t = new File("${serverJenkinsJobPath}${env['JOB_NAME']}/${env['BUILD_NUMBER']}/${defaultArtifactsPath}")
+                            println('文件测试：' + t.getPath() + "\n" + t.listFiles())
+                            if (fileTree == null || fileTree.size() == 0) {
+                                obj.title = title + "构建失败"
+                                obj.text = "详情请查看Log：${env['BUILD_URL']}console"
                             } else {
-                                filePrefixName = name
+                                fileTree.each { File file ->
+                                    def filePrefixName
+                                    if (file.name.contains(".")) {
+                                        filePrefixName = file.name.substring(0, file.name.lastIndexOf("."))
+                                    } else {
+                                        filePrefixName = name
+                                    }
+                                    contentStr.append("\n")
+                                            .append("文件：")
+                                            .append(filePrefixName)
+                                            .append(" 下载地址为：")
+                                            .append(buildUrl)
+                                            .append(defaultArtifactsPath)
+                                            .append(file.name)
+                                }
+                                obj.title = title + "构建成功"
+                                obj.text = contentStr.toString()
                             }
-                            contentStr.append("\n")
-                                    .append("文件：")
-                                    .append(filePrefixName)
-                                    .append(" 下载地址为：")
-                                    .append(buildUrl)
-                                    .append(defaultArtifactsPath)
-                                    .append(file.name)
-                        }
-                        obj.title = title + "构建成功"
-                        obj.text = contentStr.toString()
-                    }
 
-                    request.body = new Gson().toJson(obj)
-                    request.charset = 'UTF-8'
-                    request.contentType = 'application/json'
-                }.post()
-                println result
+                            request.body = new Gson().toJson(obj)
+                            request.charset = 'UTF-8'
+                            request.contentType = 'application/json'
+                        }.post()
+                        println result
+                    }
+                }).start()
             }
         }
     }
